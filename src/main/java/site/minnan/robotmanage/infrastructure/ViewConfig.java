9954b6f377949ee90fb0c2c0eec92225e829e6c9
Minnan <978312456a@gmail.com>
2024-04-20 17:05:48 +0800

    jms路径配置

--- src/main/java/site/minnan/robotmanage/infrastructure/ViewConfig.java
+++ src/main/java/site/minnan/robotmanage/infrastructure/ViewConfig.java
@@ -1,7 +1,6 @@
 package site.minnan.robotmanage.infrastructure;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
